a2fb20aa286d6e1c813f81669466c46d8b27f914
Aitor Gómez Goiri <aitor.gomez-goiri@open.ac.uk>
2015-04-01 15:59:45 +0100

    Defining video elements' orientation and styling them so they can fit in the collections' list of items.

--- js/images.js
+++ js/images.js
@@ -1,17 +1,22 @@
-function setOrientation(el) {
-	if (el.width()<el.height()) { // Portrait images!
-		el.addClass("portrait");
-	} else {
-		el.addClass("landscape");
-	}
+function getOrientation(width, height) {
+	if (width < height)
+		return "portrait";
+	return "landscape";
 }
+
 
 $(function() {
 	$(".elgg-list .elgg-body img").load(function() {
-		setOrientation($(this));
+		var orientation = getOrientation($(this).width(), $(this).height());
+		$(this).addClass(orientation);
 	});
 
-	$(".elgg-list .elgg-body video").load(function() {
-		setOrientation($(this));
+	$(".elgg-list .elgg-body video").each(function() {
+		$(this).bind("loadedmetadata", function() { //  loadedmetadata loadstart
+			var proportion = this.videoHeight / this.videoWidth
+			var orientation = getOrientation(this.videoWidth, this.videoHeight);
+			/*console.log("Orientation: " + orientation);*/
+			$(this).addClass(orientation);
+		});
 	});
 });
